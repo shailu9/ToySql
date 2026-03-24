805c4fb4f2e39b01202552d735903393f87031c1
Shailesh Joshi <shailesh.joshi090@gmail.com>
2026-03-24 17:09:22 +0530

    feat: refactored project structure by removing the duplicate parser project

--- src/ToySqlParser/Parser/AST/SelectStatement.cs
+++
@@ -1,6 +0,0 @@
-namespace ToySqlParser.Parser.AST;
-
-public record SelectStatement(List<SelectColumn> Columns, string Table, Expression? Where) : Node
-{
-    public override T Accept<T>(IAstVisitor<T> visitor) => visitor.Visit(this);
-}
